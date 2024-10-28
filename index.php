161e7c1e35b367a3fd97473510fe50e0227f5a73
srpython1 <working.of.code.in.python@gmail.com>
2024-10-28 19:59:33 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+
+//header("una noche el suburbia");
+echo "<h1>buen rap</h1>";
+echo "<h2>this in php</h2>"; 
+
+$cantante = "nach"; // text
+$topMusic = 5 ;// int
+$puntuacion = 4.8 ;// float 
+$badBoy = true ; // bolean
+
+$españa = array("nach", "posta");  // array 
+echo"el mejor de españa es ; $cantante por su puntuacion de ; $puntuancion ";
+
+
+echo"<h3> suma </h3>";
+$a = 11;
+$b = 22; 
+$suma = $a + $b ;
+echo" suma es igual ; $suma";
+
+
+echo"<h3> resta </h3>";
+$a = 11;
+$b = 22; 
+$resta = $a - $b ;
+echo" resta es igual ; $resta";
+
+echo"<h3> divicion </h3>";
+$a = 11;
+$b = 22; 
+$divicion = $a / $b ;
+echo" divicion es igual ; $divicion";
+
+echo"<h3> multiplicacion </h3>";
+$a = 11;
+$b = 22; 
+$multiplicacion = $a * $b ;
+echo" multiplicacion es igual ; $multiplicacion";
+
+
+
+
+
+
+
+
+
+
+
+
+?>
